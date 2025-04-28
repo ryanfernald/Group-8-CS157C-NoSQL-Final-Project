03249187ec9bddbc7a382c7a0f9cbddb99b73e54
Ryan <60462881+ryanfernald@users.noreply.github.com>
2025-04-28 01:51:36 -0700

    update vite config file to allow for proxy to backend mysql, not sure if working 100% but removes CORS errors

--- carrier-messenger-app/frontend/vite.config.js
+++ carrier-messenger-app/frontend/vite.config.js
@@ -3,10 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: { 
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '') 
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // This ensures Vite knows where src is
+      '@': '/src',
     },
   },
 })
