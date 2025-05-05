7e6bba68149cf9c0aa3737a298b3b34e8df38de7
Ryan <60462881+ryanfernald@users.noreply.github.com>
2025-05-04 22:28:47 -0700

    update to app.py

--- carrier-messenger-app/middle_layer/app.py
+++ carrier-messenger-app/middle_layer/app.py
@@ -1,15 +1,21 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 import threading
+from api.user_routes import user_bp
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+
+from flask_cors import CORS
+
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}})
+
+CORS(user_bp, resources={r"/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5173"]}})
 
 @app.route('/')
 def index():
     return jsonify({"message": "Carrier Messenger API is running."})
 
-# === Background Services === #
+
 def start_monitor_chats():
     from services.monitor_chats import listen_for_chat_expiry
     listen_for_chat_expiry()
@@ -18,11 +24,11 @@ def start_monitor_users():
     from services.monitor_user import monitor_user_tokens
     monitor_user_tokens()
 
-# === Launch === #
-if __name__ == "__main__":
-    # Start Redis monitor threads
+app.register_blueprint(user_bp, url_prefix='/user')
+
+if __name__ == '__main__':
+    # Start background services
     threading.Thread(target=start_monitor_chats, daemon=True).start()
     threading.Thread(target=start_monitor_users, daemon=True).start()
 
-    # Start Flask app
     app.run(debug=True, port=5000)
